5acf74331417b981305f443a54fa1f7863f369d8
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2020-11-15 17:46:25 +0000

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -41,5 +41,28 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            Seller seller = _sellerService.FindById(id.Value);
+            if(seller == null)
+            {
+                return NotFound();
+            }
+            SellerFormViewModel ViewModel = new SellerFormViewModel { Seller = seller };
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
